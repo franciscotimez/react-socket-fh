7b66991f4194b2078a150da2f22954a1a495069f
Francisco Timez <franciscotimez@gmail.com>
2023-01-14 12:42:36 -0300

    Connexion a websocket

--- 03-bandnames/bandnames/src/App.jsx
+++ 03-bandnames/bandnames/src/App.jsx
@@ -1,15 +1,46 @@
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 import { BandAdd } from "./components/BandAdd";
 import { BandList } from "./components/BandList";
 
+
+const connectSocketServer = () => {
+  const socket = io.connect('http://localhost:8080', {
+    transports: ['websocket']
+  });
+  return socket;
+};
+
 function App() {
+
+  const [socket] = useState(connectSocketServer());
+  const [online, setOnline] = useState(false);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setOnline(true);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('disconnect', () => {
+      setOnline(false);
+    });
+  }, [socket]);
+
+
   return (
     <div className="container">
 
       <div className="alert">
         <p>
-          Service status: 
-          <span className="text-success"> Online</span>
-          <span className="text-danger"> Offline</span>
+          Service status:
+          {
+            online ?
+              <span className="text-success"> Online</span>
+              :
+              <span className="text-danger"> Offline</span>
+          }
         </p>
       </div>
 
