a40bc83973f02166657e9a152ea29de7b6fdf9fa
Francisco Timez <franciscotimez@gmail.com>
2023-01-13 22:53:41 -0300

    Habilito CORS en el server

--- 01-socket-server-basic/models/server.js
+++ 01-socket-server-basic/models/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
+
 const Sockets = require('./sockets');
 
 class Server {
@@ -21,6 +23,9 @@ class Server {
   middlewares() {
     // Desplegar directorio publico
     this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+    // Habilito CORS
+    this.app.use(cors());
   }
 
   configurarSockets() {
