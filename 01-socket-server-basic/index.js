0616a81a8eb2cbfd00fc82726bab9c00ce8dc6cb
Francisco Timez <franciscotimez@gmail.com>
2023-01-13 22:43:17 -0300

    add dot env

--- 01-socket-server-basic/index.js
+++ 01-socket-server-basic/index.js
@@ -1,5 +1,6 @@
 const Server = require("./models/server");
+require('dotenv').config();
 
 const server = new Server();
 
-server.execute()
+server.execute();
--- 01-socket-server-basic/models/server.js
+++ 01-socket-server-basic/models/server.js
@@ -9,7 +9,7 @@ class Server {
 
   constructor() {
     this.app = express();
-    this.port = 8080;
+    this.port = process.env.PORT || 8080;
 
     // Http Server
     this.server = http.createServer(this.app);
