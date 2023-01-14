200b1923196983779e7da4d8a967a0becc74cadb
Francisco Timez <franciscotimez@gmail.com>
2023-01-14 14:42:33 -0300

    custom hook useSocket

--- 03-bandnames/bandnames/src/App.jsx
+++ 03-bandnames/bandnames/src/App.jsx
@@ -1,39 +1,20 @@
 import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
+
 import { BandAdd } from "./components/BandAdd";
 import { BandList } from "./components/BandList";
-
-
-const connectSocketServer = () => {
-  const socket = io.connect('http://localhost:8080', {
-    transports: ['websocket']
-  });
-  return socket;
-};
+import { useSocket } from "./hooks/useSocket";
 
 function App() {
-
-  const [socket] = useState(connectSocketServer());
-  const [online, setOnline] = useState(false);
   const [bands, setBands] = useState([]);
 
-  useEffect(() => {
-    socket.on('connect', () => {
-      setOnline(true);
-    });
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on('disconnect', () => {
-      setOnline(false);
-    });
-  }, [socket]);
+  const { socket, online } = useSocket('http://localhost:8080');
 
   useEffect(() => {
     socket.on('current-bands', (bands) => {
       setBands(bands);
     });
   }, [socket]);
+
 
   const votar = (id) => {
     socket.emit('votar-banda', id);
---
+++ 03-bandnames/bandnames/src/hooks/useSocket.js
@@ -0,0 +1,29 @@
+
+import { useEffect, useMemo, useState } from "react";
+import io from "socket.io-client";
+
+export const useSocket = (serverPath) => {
+
+  const socket = useMemo(() => io.connect(serverPath, {
+    transports: ['websocket']
+  }), [serverPath]);
+
+  const [online, setOnline] = useState(false);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setOnline(true);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('disconnect', () => {
+      setOnline(false);
+    });
+  }, [socket]);
+
+  return {
+    socket,
+    online
+  };
+};
