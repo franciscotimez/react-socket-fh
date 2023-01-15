06d64cd136eb475e630d64430d5f86c9ed7b3a17
Francisco Timez <franciscotimez@gmail.com>
2023-01-15 13:22:41 -0300

    Make React Router

--- 04-ticket-app/ticket-app/src/pages/RouterPage.jsx
+++ 04-ticket-app/ticket-app/src/pages/RouterPage.jsx
@@ -8,6 +8,11 @@ import {
   VideoCameraOutlined,
 } from '@ant-design/icons';
 import { Layout, Menu, theme } from 'antd';
+import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
+import { Ingresar } from './Ingresar';
+import { Cola } from './Cola';
+import { CrearTicket } from './CrearTicket';
+import { Escritorio } from './Escritorio';
 const { Header, Sider, Content } = Layout;
 
 export const RouterPage = () => {
@@ -18,55 +23,65 @@ export const RouterPage = () => {
   } = theme.useToken();
 
   return (
-    <Layout style={{ height: '100vh' }}>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'Ingresar',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'Cola',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'Crear Ticket',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout className="site-layout">
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger',
-            onClick: () => setCollapsed(!collapsed),
-          })}
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          Content
-        </Content>
+    <BrowserRouter>
+      <Layout style={{ height: '100vh' }}>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: '1',
+                icon: <UserOutlined />,
+                label: <Link to="/ingresar">Ingresar</Link>,
+              },
+              {
+                key: '2',
+                icon: <VideoCameraOutlined />,
+                label: <Link to="/cola">Cola</Link>,
+              },
+              {
+                key: '3',
+                icon: <UploadOutlined />,
+                label: <Link to="/crear">Crear Ticket</Link>,
+              },
+            ]}
+          />
+        </Sider>
+        <Layout className="site-layout">
+          <Header
+            style={{
+              padding: 0,
+              background: colorBgContainer,
+            }}
+          >
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: () => setCollapsed(!collapsed),
+            })}
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            <Routes>
+              <Route path="/ingresar" element={<Ingresar />} />
+              <Route path="/cola" element={<Cola />} />
+              <Route path="/crear" element={<CrearTicket />} />
+
+              <Route path="/escritorio" element={<Escritorio />} />
+              
+              <Route path="/*" element={<Navigate to="/ingresar" />} />
+            </Routes>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </BrowserRouter>
   );
 };
