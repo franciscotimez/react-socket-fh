54f3f729f881bcaaf6f33ca839c328b792e250fe
Francisco Timez <franciscotimez@gmail.com>
2023-01-15 12:38:50 -0300

    Inicio Proyecto Ticketapp

---
+++ 04-ticket-app/ticket-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'antd/dist/reset.css';
+import './index.css';
+import { TicketApp } from './TicketApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <TicketApp />
+);
