d37bdfaee58176d6e296a5e5b5a8fac6fd058f45
Francisco Timez <franciscotimez@gmail.com>
2023-01-15 12:13:48 -0300

    Add chart.js

--- 03-bandnames/bandnames/src/pages/HomePage.jsx
+++ 03-bandnames/bandnames/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { BandAdd } from "../components/BandAdd";
+import { BandChart } from "../components/BandChart";
 import { BandList } from "../components/BandList";
 import { SocketContext } from "../context/SocketContext";
 
@@ -26,6 +27,12 @@ function HomePage() {
 
       <h1>Band Names</h1>
       <hr />
+
+      <div className="row">
+        <div className="col">
+          <BandChart />
+        </div>
+      </div>
 
       <div className="row">
         <div className="col-8">
