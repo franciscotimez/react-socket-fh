70502a83347ca9c7971c583b067f9b4ce4b27da8
Francisco Timez <franciscotimez@gmail.com>
2023-01-15 15:37:11 -0300

    Crear ticket page

--- 04-ticket-app/ticket-app/src/pages/CrearTicket.jsx
+++ 04-ticket-app/ticket-app/src/pages/CrearTicket.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Button, Col, Row, Typography } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+
+const { Title, Text } = Typography;
 
 export const CrearTicket = () => {
+
+  const nuevoTicket = () => {
+    console.log('nuevoTicket');
+  };
+
   return (
-    <div>CrearTicket</div>
-  )
-}
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={3}>Presione el boton para crear un nuevo Ticket</Title>
+          <Button
+            type='primary'
+            shape='round'
+            icon={<DownloadOutlined />}
+            size='large'
+            onClick={nuevoTicket}
+          >
+            Nuevo Ticket
+          </Button>
+        </Col>
+      </Row>
+
+      <Row style={{ marginTop: 100 }}>
+        <Col span={14} offset={6} align="center">
+          <Text level={2}>
+            Su numero
+          </Text>
+          <br />
+          <Text type='success' style={{ fontSize: 55 }}>
+            55
+          </Text>
+        </Col>
+      </Row>
+    </>
+  );
+};
