a15b6904bcd1d5a71b4b1d5e50115f573595e480
Francisco Timez <franciscotimez@gmail.com>
2023-01-15 15:53:33 -0300

    Cola Page

--- 04-ticket-app/ticket-app/src/pages/Cola.jsx
+++ 04-ticket-app/ticket-app/src/pages/Cola.jsx
@@ -1,7 +1,96 @@
-import React from 'react'
+import React from 'react';
+
+import { Card, Col, Divider, List, Row, Tag, Typography } from 'antd';
+
+const { Title, Text } = Typography;
+
+const data = [
+  {
+    ticketNo: 33,
+    escritorio: 3,
+    agente: 'Fernando Herrera'
+  },
+  {
+    ticketNo: 34,
+    escritorio: 4,
+    agente: 'Melissa Flores'
+  },
+  {
+    ticketNo: 35,
+    escritorio: 5,
+    agente: 'Carlos Castro'
+  },
+  {
+    ticketNo: 36,
+    escritorio: 3,
+    agente: 'Fernando Herrera'
+  },
+  {
+    ticketNo: 37,
+    escritorio: 3,
+    agente: 'Fernando Herrera'
+  },
+  {
+    ticketNo: 38,
+    escritorio: 2,
+    agente: 'Melissa Flores'
+  },
+  {
+    ticketNo: 39,
+    escritorio: 5,
+    agente: 'Carlos Castro'
+  },
+];
 
 export const Cola = () => {
   return (
-    <div>Cola</div>
-  )
-}
+    <>
+      <Title level={1}>Atendiendo al Cliente</Title>
+      <Row>
+        <Col span={12}>
+          <List
+            dataSource={data.slice(0, 3)}
+            renderItem={item => (
+              <List.Item>
+                <Card
+                  style={{
+                    width: 300,
+                    marginTop: 16
+                  }}
+                  actions={[
+                    <Tag color="volcano">{item.agente}</Tag>,
+                    <Tag color="magenta">Escritorio:{item.escritorio}</Tag>,
+                  ]}
+                >
+                  <Title>{item.ticketNo}</Title>
+                </Card>
+              </List.Item>
+            )}
+          />
+        </Col>
+        <Col span={12}>
+          <Divider>Historial</Divider>
+          <List
+            dataSource={data.slice(3)}
+            renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  title={`Ticket No. ${item.ticketNo}`}
+                  description={
+                    <>
+                      <Text type='secondary'>En el escritorio: </Text>
+                      <Tag color='magenta'>{item.escritorio}</Tag>
+                      <br />
+                      <Text type='secondary'>Agente: </Text>
+                      <Tag color='volcano'>{item.agente}</Tag>
+                    </>
+                  }
+                />
+              </List.Item>
+            )}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
