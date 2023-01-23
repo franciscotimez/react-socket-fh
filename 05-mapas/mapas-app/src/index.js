d4f3ca802bd5725558cd9652f6c48cf6ba05827a
Francisco Timez <franciscotimez@gmail.com>
2023-01-23 17:21:18 -0300

    Inicio del proyecto install mapbox

---
+++ 05-mapas/mapas-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MapasApp } from './MapasApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MapasApp />
+  </React.StrictMode>
+);
---
+++ 05-mapas/mapas-app/src/pages/MapaPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MapaPage = () => {
+  return (
+    <div>MapaPage</div>
+  )
+}
