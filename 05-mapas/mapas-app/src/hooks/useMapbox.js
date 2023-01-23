0b83c187a72ce18393fd935c2f39fd27f502ad1e
Francisco Timez <franciscotimez@gmail.com>
2023-01-23 18:49:38 -0300

    Creacion y guardado de marcadores

--- 05-mapas/mapas-app/src/hooks/useMapbox.js
+++ 05-mapas/mapas-app/src/hooks/useMapbox.js
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import { v4 as uuidv4 } from 'uuid';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbnRpbWV6IiwiYSI6ImNsY2dqM3Y5cDNka3kzcGw3bG01b3I0encifQ.Ti2e1awq7MT8PLLkI-goSg';
 
@@ -11,6 +12,9 @@ export const useMapbox = (puntoInicial) => {
   const setRef = useCallback((node) => {
     mapaDiv.current = node;
   }, []);
+
+  // Marcadores
+  const marcadores = useRef({});
 
   // Referencia al mapa
   const mapa = useRef();
@@ -41,8 +45,27 @@ export const useMapbox = (puntoInicial) => {
     };
   }, []);
 
+  // Agregar marcador cuando se hace click
+  useEffect(() => {
+    mapa.current?.on('click', (event) => {
+      const { lng, lat } = event.lngLat;
+
+      const marker = new mapboxgl.Marker();
+      marker.id = uuidv4();
+
+      marker.setLngLat([lng, lat]).addTo(mapa.current).setDraggable(true);
+
+      marcadores.current[marker.id] = marker;
+
+    });
+    return () => {
+      mapa.current?.off('move');
+    };
+  }, []);
+
   return {
     coords,
-    setRef
+    setRef,
+    marcadores
   };
 };
