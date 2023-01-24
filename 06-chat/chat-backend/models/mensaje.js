c2e7a51c83420131efef2601b0dded30f6abd847
Francisco Timez <franciscotimez@gmail.com>
2023-01-24 18:38:16 -0300

    Definicion de esquemas y models

---
+++ 06-chat/chat-backend/models/mensaje.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const MensajeSchema = Schema(
+  {
+    from: { type: Schema.Types.ObjectId, ref: "Usuario", required: true },
+    to: { type: Schema.Types.ObjectId, ref: "Usuario", required: true },
+    message: { type: String, required: true },
+  },
+  { timestamp: true }
+);
+
+MensajeSchema.method("toJSON", function () {
+  const { __v, ...object } = this.toObject();
+  object.uid = _id;
+  return object;
+});
+
+module.exports = model("Mensaje", MensajeSchema);
