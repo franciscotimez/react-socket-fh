10510878a2776889ea1bf5707446e9e41544ffd7
Francisco Timez <franciscotimez@gmail.com>
2023-01-25 13:34:08 -0300

    Creo nuevo usuario

--- 06-chat/chat-backend/controllers/auth.js
+++ 06-chat/chat-backend/controllers/auth.js
@@ -1,10 +1,36 @@
 const { response, request } = require("express");
+const Usuario = require("../models/usuario");
 
-const crearUsuario = async (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "new",
-  });
+const crearUsuario = async (req = request, res = response) => {
+  try {
+    const { email, password } = req.body;
+
+    const existeEmail = await Usuario.findOne({ email });
+
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Email already exists",
+      });
+    }
+
+    // todo: encryptar password
+
+    // Guardar en BD
+    const newUser = new Usuario(req.body);
+
+    await newUser.save();
+
+    console.log(newUser);
+
+    res.json({ newUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 const loginUsuario = async (req = request, res = response) => {
