3f33c0c50ed5e8c32a30001930eb2259eeeadbf7
Francisco Timez <franciscotimez@gmail.com>
2023-01-25 13:20:19 -0300

    validacion custom middleware

--- 06-chat/chat-backend/controllers/auth.js
+++ 06-chat/chat-backend/controllers/auth.js
@@ -1,5 +1,4 @@
 const { response, request } = require("express");
-const { validationResult } = require("express-validator");
 
 const crearUsuario = async (req, res = response) => {
   res.json({
@@ -9,13 +8,6 @@ const crearUsuario = async (req, res = response) => {
 };
 
 const loginUsuario = async (req = request, res = response) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
   const { body } = req;
 
   res.json({
--- 06-chat/chat-backend/router/auth.js
+++ 06-chat/chat-backend/router/auth.js
@@ -9,17 +9,32 @@ const {
   loginUsuario,
   renovarToken,
 } = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validarCampos");
 
 const router = Router();
 
 // Crear nuevos usuarios
-router.post("/new", crearUsuario);
+router.post(
+  "/new",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password es obligatorio").not().isEmpty(),
+    validarCampos,
+  ],
+  crearUsuario
+);
 
 // Login
-router.post("/", [
-  check('email', 'El email es obligatorio').isEmail(),
-  check('password', 'El password es obligatorio').not().isEmpty()
-], loginUsuario);
+router.post(
+  "/",
+  [
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password es obligatorio").not().isEmpty(),
+    validarCampos,
+  ],
+  loginUsuario
+);
 
 // Renovar Token
 router.get("/renew", renovarToken);
