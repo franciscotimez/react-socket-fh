311008724d1695da482e654bcaa7cbfb559b4967
Francisco Timez <franciscotimez@gmail.com>
2023-01-27 10:08:45 -0300

    Inicio chat app

---
+++ 06-chat/chat-app/src/ChatApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ChatApp = () => {
+  return (
+    <div>ChatApp</div>
+  )
+}
---
+++ 06-chat/chat-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChatApp } from './ChatApp';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <ChatApp />
+);
+
