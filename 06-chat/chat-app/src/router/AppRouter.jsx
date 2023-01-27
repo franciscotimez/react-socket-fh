db0c973bb1c8bce647058176321d4d970854c4c3
Francisco Timez <franciscotimez@gmail.com>
2023-01-27 10:32:53 -0300

    Router Secundario auth

--- 06-chat/chat-app/src/router/AppRouter.jsx
+++ 06-chat/chat-app/src/router/AppRouter.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { ChatPage } from '../pages/ChatPage';
-import { LoginPage } from '../pages/LoginPage';
-import { RegisterPage } from '../pages/RegisterPage';
+import { AuthRouter } from './AuthRouter';
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/auth/*" element={<AuthRouter />} />
       <Route path="/" element={<ChatPage />} />
       <Route path="/*" element={<Navigate to="/" />} />
 
---
+++ 06-chat/chat-app/src/router/AuthRouter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { LoginPage } from '../pages/LoginPage';
+import { RegisterPage } from '../pages/RegisterPage';
+
+export const AuthRouter = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
+    </Routes>
+  )
+}
