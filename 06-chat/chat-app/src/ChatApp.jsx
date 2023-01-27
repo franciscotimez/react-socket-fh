db9d5dc67e4ca7e55dc397a08ce32a499a5a3e71
Francisco Timez <franciscotimez@gmail.com>
2023-01-27 18:21:57 -0300

    Conexion con socket

--- 06-chat/chat-app/src/ChatApp.jsx
+++ 06-chat/chat-app/src/ChatApp.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './auth/AuthContext';
+import { SocketProvider } from './context/SocketContext';
 import { AppRouter } from './router/AppRouter';
 
 export const ChatApp = () => {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <AppRouter />
+        <SocketProvider>
+          <AppRouter />
+        </SocketProvider>
       </AuthProvider>
     </BrowserRouter>
   );
--- 06-chat/chat-backend/models/sockets.js
+++ 06-chat/chat-backend/models/sockets.js
@@ -8,18 +8,22 @@ class Sockets {
   socketsEvents() {
     // on connection
     this.io.on("connection", (socket) => {
+      console.log("Cliente conectado => ", socket.id);
+
       // todo: Validar JWT y desconectar si no es valido
-
+      
       // todo: Saber que usuario esta activo
-
+      
       // todo: Emitir todos los usuarios conectados
-
+      
       // todo: Socket join
-
+      
       // todo: Escuchar cuando un cliente manda un mensaje-personal
-
-      // todo: Manejar el Disconect
-
+      
+      // Manejar el Disconect
+      socket.on("disconnect", () => {
+        console.log("Cliente desconectado => ", socket.id);
+      });
     });
   }
 }
